7c8203c3337788b72967020658ff07f526a7ced4
Clément REPEL <contact@clement-repel.fr>
2020-04-03 11:24:36 +0200

    Code comment

--- script/script.js
+++ script/script.js
@@ -1,12 +1,12 @@
 "use strict";
 
 
-var slideIndex = 0;
-showSlides();
+var slideIndex = 0;  // Initalisation de slideIndex à 0
+showSlides(); // Nom de la fonction
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
+function showSlides() { // Création de la fonction
+  var i; // Déclaration de la variable i
+  var slides = document.getElementsByClassName("mySlides"); // Déclaration de la variable slides par document.getElementsByClassName qui récupère la class "mySlides"
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -14,4 +14,4 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 4000); // Change l'image toute les deux secondes
-} 
+}
